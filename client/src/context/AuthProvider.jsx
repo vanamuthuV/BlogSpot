bfd2811b2c216174ce25235e093b59f3fa78999b
vanamuthuV <vanamuthuvana22@gmail.com>
2024-06-02 09:15:13 +0530

    Updated the accesstoken for reload user

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -13,14 +13,21 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     if (Object.keys(user).length === 0) {
       const ReloadUser = async () => {
+        const data = {
+          accessToken: localStorage.getItem("accessToken"),
+        };
         try {
-          const response = await axios.post(RELOAD_USER, {
+          const response = await axios.post(RELOAD_USER, data, {
             headers: {
               "Content-Type": "application/json", // Adjust the content type as needed
               Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
             },
           });
           // console.log(response?.data?.data);
+          localStorage.setItem(
+            "accessToken",
+            response?.data?.data?.accessToken
+          );
           setUser(response?.data?.data);
           setAuth(response?.data?.data);
         } catch (error) {
--- server/routes/reloaduser.js
+++ server/routes/reloaduser.js
@@ -3,20 +3,20 @@ import Authentication from "../middleware/authorization.js";
 import pool from "../db.js";
 const router = express.Router();
 
-router.post('/',Authentication , async (req, res) => {
-    console.log(req.user)
-    const { user_id } = req.user
-    
-    const query = `
+router.post("/", Authentication, async (req, res) => {
+  console.log(req.user);
+  const { user_id } = req.user;
+  const { accessToken } = req?.body?.data;
+  const query = `
         select * from users left join profilepicture on users.user_id = profilepicture.user_id where users.user_id = $1
-    `
+    `;
 
-    const Profile = await pool.query(query, [user_id])
-        const obj = Object.assign({}, req.user, Profile.rows[0]);
-    console.log(Profile.rows);
-    console.log("Hoo");
-    console.log(obj);
-    res.status(200).json({data : obj })
-})
+  const Profile = await pool.query(query, [user_id]);
+  const obj = Object.assign({}, req.user, Profile.rows[0], {accessToken : accessToken});
+  console.log(Profile.rows);
+  console.log("Hoo");
+  console.log(obj);
+  res.status(200).json({ data: obj });
+});
 
 export default router;
