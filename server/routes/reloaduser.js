123dcc7f6ebcab3b96fc3df796711a2974668269
vanamuthuV <vanamuthuvana22@gmail.com>
2024-08-20 18:14:47 +0530

    Reload User Testing

--- server/routes/reloaduser.js
+++ server/routes/reloaduser.js
@@ -5,20 +5,25 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   console.log(req.user);
-  const { user_id } = req.user;
-  const { accessToken } = req?.user;
-  const query = `
+  try {
+    const { user_id } = req.body;
+    const { accessToken } = req?.user;
+    const query = `
         select * from users left join profilepicture on users.user_id = profilepicture.user_id where users.user_id = $1
     `;
 
-  const Profile = await pool.query(query, [user_id]);
-  const obj = Object.assign({}, req.user, Profile.rows[0], {
-    accessToken: accessToken,
-  });
-  console.log(Profile.rows);
-  console.log("Hoo");
-  console.log(obj);
-  res.status(200).json({ data: obj });
+    const Profile = await pool.query(query, [user_id]);
+    const obj = Object.assign({}, req.user, Profile.rows[0], {
+      accessToken: accessToken,
+    });
+    console.log(Profile.rows);
+    console.log("Hoo");
+    console.log(obj);
+    res.status(200).json({ data: obj });
+  } catch (error) {
+    res.status(403).json({error : error})
+  }
+  
 });
 
 export default router;
