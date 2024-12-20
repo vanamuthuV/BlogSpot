9315bee44b20aededa8da133a283d448d32525c3
vanamuthuV <vanamuthuvana22@gmail.com>
2024-12-20 20:42:03 +0530

    updates

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -42,7 +42,7 @@ const EnhancedHero = () => {
               transition={{ delay: 0.6, duration: 0.8 }}
             >
               <a
-                href="/create-post"
+                href="/createpost"
                 className="px-6 py-2 font-bold text-white transition duration-300 ease-in-out transform bg-orange-500 rounded-full shadow-lg md:px-8 md:py-3 hover:bg-orange-600 hover:scale-105"
               >
                 Create Post
